a81368f1f67bf7c60f0d2855473bead5b5fa8783
Andy Glover <andy.glover@amd.com>
2022-11-24 07:43:26 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,4 @@
+import '/src/scss/styles.scss';
+// Whole Bootstrap import. You can pare this down as needed like
+// import { Tooltip, Toast, Popover } from 'bootstrap'
+import * as bootstrap from 'bootstrap';
